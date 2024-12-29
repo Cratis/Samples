2c651d84c811b24114958a724739b1977ab8f4bd
Einar Ingebrigtsen <einari@me.com>
2024-12-29 13:57:42 +0100

    Adding region for documentation

--- Chronicle/Quickstart/Common/DemoData.cs
+++ Chronicle/Quickstart/Common/DemoData.cs
@@ -6,10 +6,12 @@ public class DemoData(IEventLog eventLog)
 {
     public async Task Initialize()
     {
+        #region Snippet:Quickstart-DemoData
         await eventLog.Append(Guid.NewGuid(), new UserOnboarded("Jane Doe", "jane@interwebs.net"));
         await eventLog.Append(Guid.NewGuid(), new UserOnboarded("John Doe", "john@interwebs.net"));
 
         await eventLog.Append(Guid.NewGuid(), new BookAddedToInventory("Metaprogramming in C#: Automate your .NET development and simplify overcomplicated code", "Einar Ingebrigtsen", "978-1837635429"));
         await eventLog.Append(Guid.NewGuid(), new BookAddedToInventory("Understanding Eventsourcing: Planning and Implementing scalable Systems with Eventmodeling and Eventsourcing", "Martin Dilger", "979-8300933043"));
+        #endregion Snippet:Quickstart-DemoData
     }
 }
