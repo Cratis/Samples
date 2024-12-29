6ee9ed5b991f4a2f9bcd71608d3ed65673a33b78
Einar Ingebrigtsen <einari@me.com>
2024-12-29 13:41:24 +0100

    Adding ToString() override

--- Chronicle/Quickstart/Common/OverdueBook.cs
+++ Chronicle/Quickstart/Common/OverdueBook.cs
@@ -1,3 +1,6 @@
 namespace Quickstart.Common;
 
-public record OverdueBook(Guid Id, string Title, Guid UserId, string User, DateTimeOffset Borrowed);
+public record OverdueBook(Guid Id, string Title, Guid UserId, string User, DateTimeOffset Borrowed)
+{
+    public override string ToString() => Title;
+}
--- Chronicle/Quickstart/Common/ReservedBook.cs
+++ Chronicle/Quickstart/Common/ReservedBook.cs
@@ -1,3 +1,6 @@
 namespace Quickstart.Common;
 
-public record ReservedBook(Guid Id, Guid UserId, string Title, string User, DateTimeOffset Reserved);
+public record ReservedBook(Guid Id, Guid UserId, string Title, string User, DateTimeOffset Reserved)
+{
+    public override string ToString() => Title;
+}
