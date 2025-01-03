939dea0a685926c6f7e7c93e6fac06b0aa987192
Einar Ingebrigtsen <einari@me.com>
2025-01-03 14:46:12 +0100

    Adding a snippet for BorrowedBooks

--- Chronicle/Quickstart/Common/BorrowedBooks.cs
+++ Chronicle/Quickstart/Common/BorrowedBooks.cs
@@ -2,7 +2,9 @@ using MongoDB.Driver;
 
 namespace Quickstart.Common;
 
+#region Snippet:Quickstart-BorrowedBooks
 public class BorrowedBooks(IMongoCollection<BorrowedBook> collection)
 {
     public IEnumerable<BorrowedBook> GetAll() => collection.Find(Builders<BorrowedBook>.Filter.Empty).ToList();
 }
+#endregion Snippet:Quickstart-BorrowedBooks
