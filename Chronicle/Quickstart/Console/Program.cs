1163866d4a1174cf94c7690c4604a8d3c6b31a22
Einar Ingebrigtsen <einari@me.com>
2025-01-10 18:38:45 +0100

    Removing line

--- Chronicle/Quickstart/Console/Program.cs
+++ Chronicle/Quickstart/Console/Program.cs
@@ -11,7 +11,6 @@ using var client = new ChronicleClient("chronicle://localhost:35000");
 var eventStore = client.GetEventStore("Quickstart");
 await eventStore.DiscoverAll();
 await eventStore.RegisterAll();
-
 #endregion Snippet:Quickstart-Console-Setup
 
 MongoDBDefaults.Initialize();
