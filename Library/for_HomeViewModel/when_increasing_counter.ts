0faa4d101b414fd1a1ee452d40ad9dd0b57cc091
Einar Ingebrigtsen <einari@me.com>
2025-04-06 15:17:21 +0200

    Removing unused spec

--- Library/for_HomeViewModel/when_increasing_counter.ts
+++
@@ -1,9 +0,0 @@
-import { HomeViewModel } from '../HomeViewModel';
-
-describe('when increasing counter', () => {
-    const viewModel = new HomeViewModel(null!);
-
-    viewModel.increaseCounter();
-
-    it('should increase the counter', () => viewModel.counter.should.be.equal(1));
-});
