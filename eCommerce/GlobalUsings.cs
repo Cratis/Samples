b2eaed533ea4cb388300bfc0d46f456bf1441726
Einar Ingebrigtsen <einari@me.com>
2025-04-28 19:59:13 +0200

    Adding most common usings

--- eCommerce/GlobalUsings.cs
+++ eCommerce/GlobalUsings.cs
@@ -1,7 +1,12 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+global using Cratis.Chronicle.Aggregates;
 global using Cratis.Chronicle.Events;
+global using Cratis.Chronicle.Observation;
 global using Cratis.Chronicle.Projections;
+global using Cratis.Chronicle.Reactors;
+global using Cratis.Chronicle.Reducers;
+global using Cratis.Chronicle.Rules;
 global using Cratis.Concepts;
 global using Microsoft.AspNetCore.Mvc;
