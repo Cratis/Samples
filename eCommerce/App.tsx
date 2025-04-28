ae1c902599296f4dd7a389976a1bb0c2036bb8c3
Einar Ingebrigtsen <einari@me.com>
2025-04-28 20:01:10 +0200

    Fixing frontend scaffold

--- eCommerce/App.tsx
+++ eCommerce/App.tsx
@@ -9,12 +9,6 @@ import { ConfirmationDialog } from 'Components/Dialogs';
 import { Home } from './Home';
 import { DefaultLayout } from './Layout/Default/DefaultLayout';
 import { IMenuItemGroup } from './Layout/Default/Sidebar/MenuItem/MenuItem';
-import { Authors } from './Features/Authors/Authors';
-import * as mdIcons from 'react-icons/md';
-import * as ioIcons from 'react-icons/io5';
-import * as faIcons from 'react-icons/fa6';
-import { Lenders } from './Features/Lenders/Lenders';
-import { Books } from './Features/Books/Books';
 
 function App() {
     useTheme();
@@ -22,11 +16,6 @@ function App() {
     const menuItems: IMenuItemGroup[] = [
         {
             items: [
-                { label: 'Authors', url: '/authors', icon: mdIcons.MdPeopleAlt },
-                { label: 'Books', url: '/books/inventory', icon: mdIcons.MdBook },
-                { label: 'Reservations', url: '/books/reservations', icon: ioIcons.IoBagCheckOutline },
-                { label: 'Overdue books', url: '/books/overdue', icon: faIcons.FaRegCalendarXmark },
-                { label: 'Lenders', url: '/lenders', icon: mdIcons.MdPeopleAlt },
             ]
         }
     ];
@@ -41,9 +30,6 @@ function App() {
                             <Route path='/home' >
                                 <Route path={''} element={<Home />} />
                             </Route>
-                            <Route path='/authors' element={<Authors />}/>
-                            <Route path='/books/*' element={<Books />}/>
-                            <Route path='/lenders' element={<Lenders />}/>
                         </Route>
                     </Routes>
                 </BrowserRouter>
