f13f7c6ab1ce9288b3574bb4823807e23390d52a
Einar <einari@me.com>
2025-05-02 14:49:05 +0200

    Adding file header

--- eCommerce/Implementation/Features/Carts/Sku.cs
+++ eCommerce/Implementation/Features/Carts/Sku.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace eCommerce.Carts;
 
 public record Sku(string Value) : ConceptAs<string>(Value)
