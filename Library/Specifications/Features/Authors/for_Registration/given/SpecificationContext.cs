3da5cd5bad307ee89396cf76265bcde661439837
Einar <einari@me.com>
2025-08-18 09:54:18 +0200

    Removing explicit specification project

--- Library/Specifications/Features/Authors/for_Registration/given/SpecificationContext.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Library.given;
-
-#pragma warning disable CS0436
-
-public class SpecificationContext(ChronicleOutOfProcessFixture fixture) : IntegrationSpecificationContext<ChronicleOutOfProcessFixture, ApiWebApplicationFactory, Program>(fixture);
