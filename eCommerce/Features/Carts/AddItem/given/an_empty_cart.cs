87005114ec7c61813716def2fbc6f3b9af85cd0b
woksin <sindrewilting@gmail.com>
2026-02-08 10:52:22 +0100

    Remove eCommerce

--- eCommerce/Features/Carts/AddItem/given/an_empty_cart.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using eCommerce;
-using eCommerce.given;
-
-namespace eCommerce.Carts.AddItem.given;
-
-public class an_empty_cart(ChronicleOutOfProcessFixture fixture) : an_http_client(fixture);
